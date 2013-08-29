b7bef0949a14944cbad1a1f877f1752e2c867851
Vasil Stoychev <ludesign.bg@gmail.com>
2013-08-29 19:34:54 +0300

    added empty unicode aware winmain function
    entry point of the program

---
+++ DirectX10/C++/main.cpp
@@ -0,0 +1,10 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	return 0;
+}
